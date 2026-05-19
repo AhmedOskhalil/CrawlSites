3b90f33f3e04aee6eb131da710a6b6e4df8dc401
AhmedKhaliel1099 <ahmedosama@78sworks.com>
2026-05-19 17:00:35 +0300

    edit ui fix problems

--- Crawl/Services/Crawl/CrawlService.cs
+++ Crawl/Services/Crawl/CrawlService.cs
@@ -93,86 +93,96 @@ namespace Crawl.Services.Crawl
 
             return partitionMatches;
         }
-        public async Task<Models.TeamInformation> GetTeamInformationAsync(int teamId)
+        public async Task<TeamInformation> GetTeamInformationAsync(int teamId)
         {
-            var url = $"https://www.filgoal.com/teams/{teamId}";
-
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Add("referer", "https://www.google.com/");
-            request.Headers.Add("user-agent",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-                "(KHTML, like Gecko) Chrome/120.0 Safari/537.36");
-
-            var response = await _httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-
-            var html = await response.Content.ReadAsStringAsync();
-
-            var doc = new HtmlDocument();
-            doc.LoadHtml(html);
-
-            var team = new Models.TeamInformation();
-            team.RecentMatches = new List<RecentMatch>();
-
-            team.TeamName = doc.DocumentNode.SelectSingleNode("//h1")?.InnerText?.Trim();
-
-            var logoNode = doc.DocumentNode
-                .SelectSingleNode("//img[contains(@class,'logo')]");
-
-            team.TeamLogo = logoNode?.GetAttributeValue("src", null);
-
-            if (!string.IsNullOrEmpty(team.TeamLogo) && team.TeamLogo.StartsWith("//"))
-                team.TeamLogo = "https:" + team.TeamLogo;
-            team.TeamOrder ??= new TeamOrder();
-
-            var orderNode = doc.DocumentNode.SelectSingleNode("//div[@data-group-id='#champ1503']");
-            var orderHeaderInfo = orderNode?.SelectSingleNode(".//div[@class='fg_rw']")?.SelectNodes(".//div");
-            var headers = orderHeaderInfo?.Select(h => h.InnerText.Trim()).ToArray();
-
-            var orderValueInfo = orderNode?.SelectSingleNode(".//div[@class='fg_rw s']")?.SelectNodes(".//div");
-            var values = orderValueInfo?.Select(h => h.InnerText.Trim()).ToArray() ?? Array.Empty<string>();
-            team.TeamOrder.Headers = headers ?? Array.Empty<string>();
-            team.TeamOrder.Values = values;
-
-            var matchNodes = doc.DocumentNode
-                .SelectNodes("//div[@class='cmim']");
-
-            if (matchNodes != null)
+            try
             {
-                foreach (var node in matchNodes.Take(2))
+                var url = $"https://www.filgoal.com/teams/{teamId}";
+
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("referer", "https://www.google.com/");
+                request.Headers.Add("user-agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                    "(KHTML, like Gecko) Chrome/120.0 Safari/537.36");
+
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var html = await response.Content.ReadAsStringAsync();
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                var team = new TeamInformation();
+                team.RecentMatches = new List<RecentMatch>();
+
+                team.TeamName = doc.DocumentNode.SelectSingleNode("//h1")?.InnerText?.Trim();
+
+                var logoNode = doc.DocumentNode
+                    .SelectSingleNode("//img[contains(@class,'logo')]");
+
+                team.TeamLogo = logoNode?.GetAttributeValue("src", null);
+
+                if (!string.IsNullOrEmpty(team.TeamLogo) && team.TeamLogo.StartsWith("//"))
+                    team.TeamLogo = "https:" + team.TeamLogo;
+                team.TeamOrder ??= new TeamOrder();
+                team.TeamOrder.Values = Array.Empty<string>();
+                var orderNodes = doc.DocumentNode.SelectNodes("//div[contains(@data-group-id, '#')]");
+                
+                foreach (var orderNode in orderNodes)
                 {
-                    var matchDoc = new HtmlDocument();
-                    matchDoc.LoadHtml(node.InnerHtml);
-
-                    var match = new RecentMatch
-                    {
-                        HomeTeamName = team.TeamName,
-
-                        AwayTeamName = matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']")?.InnerText.Trim().Split("      ")[^1] != team.TeamName ?
-                        matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']")?.InnerText.Trim().Split("      ")[^1] :
-                        matchDoc.DocumentNode.SelectNodes("//div[@class='mims']")[1]?.InnerText.Trim().Split("      ")[^1],
-
-                        MatchResult = matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']").InnerText.Trim()[0]
-                            + " - " + matchDoc.DocumentNode.SelectNodes("//div[@class='mims']")[1]?.InnerText.Trim()[0],
-
-                        MatchDate = matchDoc.DocumentNode.SelectSingleNode("//span[contains(text(),':')]")?.InnerText.Trim(),
-
-                        HomeTeamLogo = matchDoc.DocumentNode.SelectSingleNode("//div[@class='s']//img")?.GetAttributeValue("data-src", null),
-
-                        AwayTeamLogo = matchDoc.DocumentNode.SelectSingleNode("//div[@class='f']//img")?.GetAttributeValue("data-src", null)
-                    };
-
-                    if (!string.IsNullOrEmpty(match.HomeTeamLogo) && match.HomeTeamLogo.StartsWith("//"))
-                        match.HomeTeamLogo = "http:" + match.HomeTeamLogo;
-
-                    if (!string.IsNullOrEmpty(match.AwayTeamLogo) && match.AwayTeamLogo.StartsWith("//"))
-                        match.AwayTeamLogo = "http:" + match.AwayTeamLogo;
-
-                    team.RecentMatches.Add(match);
+                    var orderValueInfo = orderNode?.SelectSingleNode(".//div[@class='fg_rw s']")?.SelectNodes(".//div");
+                    team.TeamOrder.Values = orderValueInfo?.Select(h => h.InnerText.Trim()).ToArray() ?? Array.Empty<string>();
+                    break;
                 }
+
+
+                var matchNodes = doc.DocumentNode
+                    .SelectNodes("//div[@class='cmim']");
+
+                if (matchNodes != null)
+                {
+                    foreach (var node in matchNodes.Take(2))
+                    {
+                        var matchDoc = new HtmlDocument();
+                        matchDoc.LoadHtml(node.InnerHtml);
+
+                        var match = new RecentMatch
+                        {
+                            HomeTeamName = team.TeamName,
+
+                            AwayTeamName = matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']")?.InnerText.Trim().Split("      ")[^1] != team.TeamName ?
+                            matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']")?.InnerText.Trim().Split("      ")[^1] :
+                            matchDoc.DocumentNode.SelectNodes("//div[@class='mims']")[1]?.InnerText.Trim().Split("      ")[^1],
+
+                            MatchResult = matchDoc.DocumentNode.SelectSingleNode("//div[@class='mims']").InnerText.Trim()[0]
+                                + " - " + matchDoc.DocumentNode.SelectNodes("//div[@class='mims']")[1]?.InnerText.Trim()[0],
+
+                            MatchDate = matchDoc.DocumentNode.SelectSingleNode("//span[contains(text(),':')]")?.InnerText.Trim(),
+
+                            HomeTeamLogo = matchDoc.DocumentNode.SelectSingleNode("//div[@class='s']//img")?.GetAttributeValue("data-src", null),
+
+                            AwayTeamLogo = matchDoc.DocumentNode.SelectSingleNode("//div[@class='f']//img")?.GetAttributeValue("data-src", null)
+                        };
+
+                        if (!string.IsNullOrEmpty(match.HomeTeamLogo) && match.HomeTeamLogo.StartsWith("//"))
+                            match.HomeTeamLogo = "http:" + match.HomeTeamLogo;
+
+                        if (!string.IsNullOrEmpty(match.AwayTeamLogo) && match.AwayTeamLogo.StartsWith("//"))
+                            match.AwayTeamLogo = "http:" + match.AwayTeamLogo;
+
+                        team.RecentMatches.Add(match);
+                    }
+                }
+                team.teamId = teamId;
+                return team;
             }
-            team.teamId = teamId;
-            return team;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+           
         }
 
         public async Task<Models.TeamMatches> GetTeamMatchesAsync(int teamId)
